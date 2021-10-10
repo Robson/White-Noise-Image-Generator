b62c85fd01e5bb3c8e550b7e66b45a5a37cc59ef
Robson <relative@gmail.com>
2021-10-10 15:33:18 +0100

    Reduce complexity

--- page.js
+++ page.js
@@ -31,42 +31,54 @@ function generate() {
 	document.getElementById('output').src = dataURL;
 }
 
+function setMonochrome(context, size, x, y, bias) {
+	context.fillStyle = '#000000';
+	var isDrawing = Math.random() > .5;
+	var chance = { verylight: .833, light: .666, none: .5, dark: .333, verydark: .166 };
+	isDrawing = Math.random() > chance[bias];
+	if (isDrawing) {						
+		context.fillRect(x * size, y * size, size, size);
+	}
+}
+
+function setGreyscale(context, size, x, y, bias) {
+	var ranges = { verylight: [255 / 2, 255], light: [255 / 4, 255], none: [0, 255], dark: [0, 255 / 2], verydark: [0, 255 / 4]}
+	var start, end;
+	[start, end] = ranges[bias];
+	var grey = rand(start, end);
+	context.fillStyle = 'rgb(' + grey + ',' + grey + ',' + grey + ')';
+	context.fillRect(x * size, y * size, size, size);	
+}
+
+function setRgb3Bit(context, size, x, y, bias) {
+	context.fillStyle = 'rgb(' + rand(0, 1) * 255 + ',' + rand(0, 1) * 255 + ',' + rand(0, 1) * 255 + ')';
+	switch (bias) {
+		case 'verylight':
+			context.fillStyle = randArray([ '#0FF', '#F0F', '#FF0', '#FFF' ]);
+			break;
+		case 'light':
+			context.fillStyle = randArray([ '#0FF', '#F0F', '#FF0', '#FFF', '#0FF', '#F0F', '#FF0', '#FFF', '#F00', '#0F0', '#00F' ]);
+			break;
+		case 'dark':
+			context.fillStyle = randArray([ '#0FF', '#F0F', '#FF0', '#000', '#F00', '#0F0', '#00F', '#000', '#F00', '#0F0', '#00F' ]);
+			break;
+		case 'verydark':
+			context.fillStyle = randArray([ '#F00', '#0F0', '#00F', '#000' ]);
+			break;							
+	}					
+	context.fillRect(x * size, y * size, size, size);
+}
+
 function handleColours(colours, context, size, x, y, bias) {
 	switch (colours) {
 		case 'monochrome':
-			context.fillStyle = '#000000';
-			var isDrawing = Math.random() > .5;
-			var chance = { verylight: .833, light: .666, none: .5, dark: .333, verydark: .166 };
-			isDrawing = Math.random() > chance[bias];
-			if (isDrawing) {						
-				context.fillRect(x * size, y * size, size, size);
-			}
+			setMonochrome(context, size, x, y, bias);
 			break;
 		case 'greyscale':
-			var ranges = { verylight: [255 / 2, 255], light: [255 / 4, 255], none: [0, 255], dark: [0, 255 / 2], verydark: [0, 255 / 4]}
-			var start, end;
-			[start, end] = ranges[bias];
-			var grey = rand(start, end);
-			context.fillStyle = 'rgb(' + grey + ',' + grey + ',' + grey + ')';
-			context.fillRect(x * size, y * size, size, size);
+			setGreyscale(context, size, x, y, bias);
 			break;	
 		case 'rgb_3bit':
-			context.fillStyle = 'rgb(' + rand(0, 1) * 255 + ',' + rand(0, 1) * 255 + ',' + rand(0, 1) * 255 + ')';
-			switch (bias) {
-				case 'verylight':
-					context.fillStyle = randArray([ '#0FF', '#F0F', '#FF0', '#FFF' ]);
-					break;
-				case 'light':
-					context.fillStyle = randArray([ '#0FF', '#F0F', '#FF0', '#FFF', '#0FF', '#F0F', '#FF0', '#FFF', '#F00', '#0F0', '#00F' ]);
-					break;
-				case 'dark':
-					context.fillStyle = randArray([ '#0FF', '#F0F', '#FF0', '#000', '#F00', '#0F0', '#00F', '#000', '#F00', '#0F0', '#00F' ]);
-					break;
-				case 'verydark':
-					context.fillStyle = randArray([ '#F00', '#0F0', '#00F', '#000' ]);
-					break;							
-			}					
-			context.fillRect(x * size, y * size, size, size);
+			setRgb3Bit(context, size, x, y, bias);
 			break;
 		case 'rgb_6bit':
 			var ranges = { verylight: [2, 3], light: [1, 3], none: [0, 3], dark: [0, 2], verydark: [0, 1]}
