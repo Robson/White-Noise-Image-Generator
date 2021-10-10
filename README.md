9a1f2e680b1ccd443b0b8f5c38e019beaed42090
Robson <relative@gmail.com>
2021-10-10 15:14:07 +0100

    Add CodeFactor to readme + reduce complexity of function

--- README.md
+++ README.md
@@ -63,4 +63,7 @@ This is built using:
 ## Validation
    
 <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Frobson.plus%2Fwhite-noise-image-generator%2F"><img src="https://www.w3.org/Icons/valid-html401-blue" alt="Valid HTML" /></a>
+
 <a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Frobson.plus%2Fwhite-noise-image-generator%2Fstyle.css&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en"><img src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS" /></a>   	    
+
+[![X](https://www.codefactor.io/repository/github/robson/White-Noise-Image-Generator/badge?style=flat-square)](https://www.codefactor.io/repository/github/robson/White-Noise-Image-Generator)
--- page.js
+++ page.js
@@ -23,225 +23,229 @@ function generate() {
 
 	for (var x = 0; x < lengthH; x++) {
 		for (var y = 0; y < lengthV; y++) {
-			switch (colours) {
-				case 'monochrome':
-					context.fillStyle = '#000000';
-					var isDrawing = Math.random() > .5;
-					switch (bias) {
-						case 'verylight':
-							isDrawing = Math.random() > .833;
-							break;
-						case 'light':
-							isDrawing = Math.random() > .666;
-							break;
-						case 'dark':
-							isDrawing = Math.random() > .333;
-							break;
-						case 'verydark':
-							isDrawing = Math.random() > .166;
-							break;							
-					}
-					if (isDrawing) {						
-						context.fillRect(x * size, y * size, size, size);
-					}							
-					break;
-				case 'greyscale':
-					var start = 0, end = 255;
-					switch (bias) {
-						case 'verylight':
-							[start, end] = [255 / 2, 255];
-							break;
-						case 'light':
-							[start, end] = [255 / 4, 255];
-							break;
-						case 'dark':
-							[start, end] = [0, 255 / 2];
-							break;
-						case 'verydark':
-							[start, end] = [0, 255 / 4];
-							break;							
-					}					
-					var grey = rand(start, end);
-					context.fillStyle = 'rgb(' + grey + ',' + grey + ',' + grey + ')';
-					context.fillRect(x * size, y * size, size, size);
-					break;	
-				case 'rgb_3bit':
-					context.fillStyle = 'rgb(' + rand(0, 1) * 255 + ',' + rand(0, 1) * 255 + ',' + rand(0, 1) * 255 + ')';
-					switch (bias) {
-						case 'verylight':
-							context.fillStyle = randArray([ '#0FF', '#F0F', '#FF0', '#FFF' ]);
-							break;
-						case 'light':
-							context.fillStyle = randArray([ '#0FF', '#F0F', '#FF0', '#FFF', '#0FF', '#F0F', '#FF0', '#FFF', '#F00', '#0F0', '#00F' ]);
-							break;
-						case 'dark':
-							context.fillStyle = randArray([ '#0FF', '#F0F', '#FF0', '#000', '#F00', '#0F0', '#00F', '#000', '#F00', '#0F0', '#00F' ]);
-							break;
-						case 'verydark':
-							context.fillStyle = randArray([ '#F00', '#0F0', '#00F', '#000' ]);
-							break;							
-					}					
-					context.fillRect(x * size, y * size, size, size);
-					break;
-				case 'rgb_6bit':
-					var start = 0, end = 3;
-					switch (bias) {
-						case 'verylight':
-							[start, end] = [2, 3];
-							break;
-						case 'light':
-							[start, end] = [1, 3];
-							break;
-						case 'dark':
-							[start, end] = [0, 2];
-							break;
-						case 'verydark':
-							[start, end] = [0, 1];
-							break;							
-					}					
-					context.fillStyle = 'rgb(' + rand(start, end) * 85 + ',' + rand(start, end) * 85 + ',' + rand(start, end) * 85 + ')';
-					context.fillRect(x * size, y * size, size, size);
-					break;								
-				case 'rgb_12bit':
-					var start = 0, end = 5;
-					switch (bias) {
-						case 'verylight':
-							[start, end] = [3, 5];
-							break;
-						case 'light':
-							[start, end] = [1, 5];
-							break;
-						case 'dark':
-							[start, end] = [0, 4];
-							break;
-						case 'verydark':
-							[start, end] = [0, 2];
-							break;							
-					}
-					context.fillStyle = 'rgb(' + rand(start, end) * 51 + ',' + rand(start, end) * 51 + ',' + rand(start, end) * 51 + ')';
-					context.fillRect(x * size, y * size, size, size);
-					break;								
-				case 'rgb_24bit':
-					var start = 0, end = 255;
-					switch (bias) {
-						case 'verylight':
-							[start, end] = [170, 255];
-							break;
-						case 'light':
-							[start, end] = [128, 255];
-							break;
-						case 'dark':
-							[start, end] = [0, 128];
-							break;
-						case 'verydark':
-							[start, end] = [0, 85];
-							break;							
-					}				
-					context.fillStyle = 'rgb(' + rand(start, end) + ',' + rand(start, end) + ',' + rand(start, end) + ')';
-					context.fillRect(x * size, y * size, size, size);
-					break;
-				case 'red':
-					var start = 0, end = 255;
-					switch (bias) {
-						case 'verylight':
-							[start, end] = [170, 255];
-							break;
-						case 'light':
-							[start, end] = [128, 255];
-							break;
-						case 'dark':
-							[start, end] = [0, 128];
-							break;
-						case 'verydark':
-							[start, end] = [0, 85];
-							break;							
-					}				
-					context.fillStyle = 'rgb(' + rand(start, end) + ',0,0)';
-					context.fillRect(x * size, y * size, size, size);
-					break;						
-				case 'green':
-					var start = 0, end = 255;
-					switch (bias) {
-						case 'verylight':
-							[start, end] = [170, 255];
-							break;
-						case 'light':
-							[start, end] = [128, 255];
-							break;
-						case 'dark':
-							[start, end] = [0, 128];
-							break;
-						case 'verydark':
-							[start, end] = [0, 85];
-							break;							
-					}				
-					context.fillStyle = 'rgb(0,' + rand(start, end) + ',0)';
-					context.fillRect(x * size, y * size, size, size);
-					break;	
-				case 'blue':
-					var start = 0, end = 255;
-					switch (bias) {
-						case 'verylight':
-							[start, end] = [170, 255];
-							break;
-						case 'light':
-							[start, end] = [128, 255];
-							break;
-						case 'dark':
-							[start, end] = [0, 128];
-							break;
-						case 'verydark':
-							[start, end] = [0, 85];
-							break;							
-					}				
-					context.fillStyle = 'rgb(0,0,' + rand(start, end) + ')';
-					context.fillRect(x * size, y * size, size, size);
-					break;	
-				case 'space_greyscale':
-					var start = 0, end = 30;
-					switch (bias) {
-						case 'verylight':
-							[start, end] = [10, 50];
-							break;
-						case 'light':
-							[start, end] = [0, 40];
-							break;
-						case 'dark':
-							[start, end] = [0, 20];
-							break;
-						case 'verydark':
-							[start, end] = [0, 10];
-							break;							
-					}				
-					var grey = rand(start, end);
-					context.fillStyle = 'rgb(' + grey + ',' + grey + ',' + grey + ')';
-					context.fillRect(x * size, y * size, size, size);
-					break;	
-				case 'space_all':
-					var start = 0, end = 30;
-					switch (bias) {
-						case 'verylight':
-							[start, end] = [10, 50];
-							break;
-						case 'light':
-							[start, end] = [0, 40];
-							break;
-						case 'dark':
-							[start, end] = [0, 20];
-							break;
-						case 'verydark':
-							[start, end] = [0, 10];
-							break;							
-					}								
-					context.fillStyle = 'rgb(' + rand(start, end) + ',' + rand(start, end) + ',' + rand(start, end) + ')';
-					context.fillRect(x * size, y * size, size, size);
-					break;									
-			}
+			handleColours(colours, context, size, x, y, bias);
 		}
 	}
 	
 	var dataURL = canvas.toDataURL();
 	document.getElementById('output').src = dataURL;
+}
+
+function handleColours(colours, context, size, x, y, bias) {
+	switch (colours) {
+		case 'monochrome':
+			context.fillStyle = '#000000';
+			var isDrawing = Math.random() > .5;
+			switch (bias) {
+				case 'verylight':
+					isDrawing = Math.random() > .833;
+					break;
+				case 'light':
+					isDrawing = Math.random() > .666;
+					break;
+				case 'dark':
+					isDrawing = Math.random() > .333;
+					break;
+				case 'verydark':
+					isDrawing = Math.random() > .166;
+					break;							
+			}
+			if (isDrawing) {						
+				context.fillRect(x * size, y * size, size, size);
+			}							
+			break;
+		case 'greyscale':
+			var start = 0, end = 255;
+			switch (bias) {
+				case 'verylight':
+					[start, end] = [255 / 2, 255];
+					break;
+				case 'light':
+					[start, end] = [255 / 4, 255];
+					break;
+				case 'dark':
+					[start, end] = [0, 255 / 2];
+					break;
+				case 'verydark':
+					[start, end] = [0, 255 / 4];
+					break;							
+			}					
+			var grey = rand(start, end);
+			context.fillStyle = 'rgb(' + grey + ',' + grey + ',' + grey + ')';
+			context.fillRect(x * size, y * size, size, size);
+			break;	
+		case 'rgb_3bit':
+			context.fillStyle = 'rgb(' + rand(0, 1) * 255 + ',' + rand(0, 1) * 255 + ',' + rand(0, 1) * 255 + ')';
+			switch (bias) {
+				case 'verylight':
+					context.fillStyle = randArray([ '#0FF', '#F0F', '#FF0', '#FFF' ]);
+					break;
+				case 'light':
+					context.fillStyle = randArray([ '#0FF', '#F0F', '#FF0', '#FFF', '#0FF', '#F0F', '#FF0', '#FFF', '#F00', '#0F0', '#00F' ]);
+					break;
+				case 'dark':
+					context.fillStyle = randArray([ '#0FF', '#F0F', '#FF0', '#000', '#F00', '#0F0', '#00F', '#000', '#F00', '#0F0', '#00F' ]);
+					break;
+				case 'verydark':
+					context.fillStyle = randArray([ '#F00', '#0F0', '#00F', '#000' ]);
+					break;							
+			}					
+			context.fillRect(x * size, y * size, size, size);
+			break;
+		case 'rgb_6bit':
+			var start = 0, end = 3;
+			switch (bias) {
+				case 'verylight':
+					[start, end] = [2, 3];
+					break;
+				case 'light':
+					[start, end] = [1, 3];
+					break;
+				case 'dark':
+					[start, end] = [0, 2];
+					break;
+				case 'verydark':
+					[start, end] = [0, 1];
+					break;							
+			}					
+			context.fillStyle = 'rgb(' + rand(start, end) * 85 + ',' + rand(start, end) * 85 + ',' + rand(start, end) * 85 + ')';
+			context.fillRect(x * size, y * size, size, size);
+			break;								
+		case 'rgb_12bit':
+			var start = 0, end = 5;
+			switch (bias) {
+				case 'verylight':
+					[start, end] = [3, 5];
+					break;
+				case 'light':
+					[start, end] = [1, 5];
+					break;
+				case 'dark':
+					[start, end] = [0, 4];
+					break;
+				case 'verydark':
+					[start, end] = [0, 2];
+					break;							
+			}
+			context.fillStyle = 'rgb(' + rand(start, end) * 51 + ',' + rand(start, end) * 51 + ',' + rand(start, end) * 51 + ')';
+			context.fillRect(x * size, y * size, size, size);
+			break;								
+		case 'rgb_24bit':
+			var start = 0, end = 255;
+			switch (bias) {
+				case 'verylight':
+					[start, end] = [170, 255];
+					break;
+				case 'light':
+					[start, end] = [128, 255];
+					break;
+				case 'dark':
+					[start, end] = [0, 128];
+					break;
+				case 'verydark':
+					[start, end] = [0, 85];
+					break;							
+			}				
+			context.fillStyle = 'rgb(' + rand(start, end) + ',' + rand(start, end) + ',' + rand(start, end) + ')';
+			context.fillRect(x * size, y * size, size, size);
+			break;
+		case 'red':
+			var start = 0, end = 255;
+			switch (bias) {
+				case 'verylight':
+					[start, end] = [170, 255];
+					break;
+				case 'light':
+					[start, end] = [128, 255];
+					break;
+				case 'dark':
+					[start, end] = [0, 128];
+					break;
+				case 'verydark':
+					[start, end] = [0, 85];
+					break;							
+			}				
+			context.fillStyle = 'rgb(' + rand(start, end) + ',0,0)';
+			context.fillRect(x * size, y * size, size, size);
+			break;						
+		case 'green':
+			var start = 0, end = 255;
+			switch (bias) {
+				case 'verylight':
+					[start, end] = [170, 255];
+					break;
+				case 'light':
+					[start, end] = [128, 255];
+					break;
+				case 'dark':
+					[start, end] = [0, 128];
+					break;
+				case 'verydark':
+					[start, end] = [0, 85];
+					break;							
+			}				
+			context.fillStyle = 'rgb(0,' + rand(start, end) + ',0)';
+			context.fillRect(x * size, y * size, size, size);
+			break;	
+		case 'blue':
+			var start = 0, end = 255;
+			switch (bias) {
+				case 'verylight':
+					[start, end] = [170, 255];
+					break;
+				case 'light':
+					[start, end] = [128, 255];
+					break;
+				case 'dark':
+					[start, end] = [0, 128];
+					break;
+				case 'verydark':
+					[start, end] = [0, 85];
+					break;							
+			}				
+			context.fillStyle = 'rgb(0,0,' + rand(start, end) + ')';
+			context.fillRect(x * size, y * size, size, size);
+			break;	
+		case 'space_greyscale':
+			var start = 0, end = 30;
+			switch (bias) {
+				case 'verylight':
+					[start, end] = [10, 50];
+					break;
+				case 'light':
+					[start, end] = [0, 40];
+					break;
+				case 'dark':
+					[start, end] = [0, 20];
+					break;
+				case 'verydark':
+					[start, end] = [0, 10];
+					break;							
+			}				
+			var grey = rand(start, end);
+			context.fillStyle = 'rgb(' + grey + ',' + grey + ',' + grey + ')';
+			context.fillRect(x * size, y * size, size, size);
+			break;	
+		case 'space_all':
+			var start = 0, end = 30;
+			switch (bias) {
+				case 'verylight':
+					[start, end] = [10, 50];
+					break;
+				case 'light':
+					[start, end] = [0, 40];
+					break;
+				case 'dark':
+					[start, end] = [0, 20];
+					break;
+				case 'verydark':
+					[start, end] = [0, 10];
+					break;							
+			}								
+			context.fillStyle = 'rgb(' + rand(start, end) + ',' + rand(start, end) + ',' + rand(start, end) + ')';
+			context.fillRect(x * size, y * size, size, size);
+			break;									
+	}
 }
 
 function stats() {
